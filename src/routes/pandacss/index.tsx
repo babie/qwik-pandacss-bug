dd727896d9e9486f39c376d6c22c3188ed283940
babie <babie.wildheart@gmail.com>
2024-02-16 11:24:08 +0900

    use `stack.raw()` and had an error

--- src/routes/pandacss/index.tsx
+++ src/routes/pandacss/index.tsx
@@ -1,5 +1,15 @@
 import { component$ } from "@builder.io/qwik";
 import { css } from "~/styled-system/css";
+import { stack } from "~/styled-system/patterns";
+
+const headerStyles = css(
+  {},
+  stack.raw({
+    w: "100%",
+    h: "4rem",
+    bg: "red.900",
+  })
+);
 
 export default component$(() => {
   return (
@@ -12,6 +22,7 @@ export default component$(() => {
         fontSize: 30,
       })}
     >
+      <header class={headerStyles}>This is header.</header>
       This box is styled with PandaCSS.
     </div>
   );
